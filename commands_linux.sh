676a0e05242a54069806c41061d53916fcd2c530
Christian Esteban Calderón Acurio <128630513+athenidoru@users.noreply.github.com>
2026-04-02 19:08:07 +0000

    commit

--- commands_linux.sh
+++ commands_linux.sh
@@ -2,6 +2,10 @@
     2  sudo apt update
     3  sudo apt upgrade
     4  sudo apt install nmap
+    
+    4  sudo apt install nmap -y
+    #  sirve para aceptar todo sin que pregunte la terminal.
+    
     5  man
     6  man nmap
     7  history

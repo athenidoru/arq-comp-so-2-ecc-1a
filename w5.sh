d77caacee4dc16cd7579607f0775bcb62103a598
Christian Esteban Calderón Acurio <128630513+athenidoru@users.noreply.github.com>
2026-04-07 15:12:40 +0000

    wip: my first script

---
+++ w5.sh
@@ -0,0 +1,17 @@
+# lista de archivos y directorios en la carpeta actual.
+ls 
+
+# muestra el contenido de una archivo en la terminal.
+cat README.md
+
+# crea un directorio bajo el directorio en el que estoy.
+mkdir padre1.txt
+
+# cambiamos de directorio
+cd padre1.txt
+
+# creamos un archivo
+touch hijo1.txt
+
+# me regreso al directorio anterior
+cd ..
